eab2ee490e300108d4d59613fa4ed2a1a5fa0c7b
AllhadAJ22 <flutterallhad@gmail.com>
2024-03-05 20:35:48 +0530

    0229

--- 0229_login/login_page/lib/login.dart
+++ 0229_login/login_page/lib/login.dart
@@ -8,6 +8,11 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController userName = TextEditingController();
+  TextEditingController passWord = TextEditingController();
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,26 +21,94 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.amberAccent,
         title: const Text("Login Page"),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "what is your UserName ?",
-              // label: const Text("UserName"),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
+      body: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: userName,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.person),
+                hintText: "Enter your UserName ?",
+                label: const Text("UserName"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              // keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                print("username");
+                if (value == null || value.isEmpty) {
+                  return "please enter username";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              obscureText: true,
+              obscuringCharacter: "@",
+              controller: passWord,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove_red_eye_outlined),
+                ),
+                hintText: "Enter your PassWord ?",
+                label: const Text("Password"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              validator: (value) {
+                print("password");
+                if (value == null || value.isEmpty) {
+                  return "please enter password";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                bool validate = _formkey.currentState!.validate();
+                if (validate) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("LOGIN SUCCESSFUL"),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("LOGIN FAAILED"),
+                    ),
+                  );
+                }
+              },
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                fixedSize: MaterialStatePropertyAll(Size(300, 50)),
+              ),
+              child: const Text(
+                "Submit",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          TextFormField(),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Submit"),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
